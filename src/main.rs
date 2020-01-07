c0ab8f850137c8b8d13c3d9717d0bdcf3c354bb3
Richard Dallaway <richard@dallaway.com>
2020-01-07 17:44:43 +0000

    implement body text extraction

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,32 @@ use structopt::StructOpt;
 mod settings;
 use settings::Settings;
 
+fn main() {
+    let settings = Settings::from_args();
+
+    match fetch(&settings) {
+        Err(err) => stop(err),
+        Ok(None) => complete(0),
+        Ok(Some(mime_message)) => match parse_mail(mime_message.as_bytes()).and_then(extract) {
+            Err(err) => stop(err),
+            Ok(info) => {
+                println!("{:?}", info);
+                complete(1)
+            }
+        },
+    }
+}
+
+fn stop<E: std::fmt::Debug>(err: E) -> ! {
+    eprintln!("{:?}", err);
+    std::process::exit(1)
+}
+
+fn complete(num_msgs: usize) -> ! {
+    println!("{}", num_msgs);
+    std::process::exit(0)
+}
+
 fn describe(mail: &ParsedMail) {
     println!("Found: {:?}", &mail.ctype);
     let parts = &mail.subparts;
@@ -19,6 +45,7 @@ fn describe(mail: &ParsedMail) {
 struct PostInfo {
     title: String,
     author: String,
+    content: String,
     //date:
     //attachments:
     //content:
@@ -41,35 +68,36 @@ fn extract(mail: ParsedMail) -> Result<PostInfo, MailParseError> {
     let title = subject.unwrap_or(String::from("untitled"));
     let author = sender_text.unwrap_or(String::from("someone"));
 
-    Ok(PostInfo { title, author })
+    Ok(PostInfo { 
+        title, 
+        author,
+        content: body(&mail)?.unwrap_or(String::from("shrug")), 
+    })
 }
 
-fn stop<E: std::fmt::Debug>(err: E) -> ! {
-    eprintln!("{:?}", err);
-    std::process::exit(1)
-}
+fn body(mail: &ParsedMail) -> Result<Option<String>, MailParseError> {
 
-fn complete(num_msgs: usize) -> ! {
-    println!("{}", num_msgs);
-    std::process::exit(0)
-}
-
-// Output the number of emails processed (usually 0)
-fn main() {
-    let settings = Settings::from_args();
-
-    match fetch(&settings) {
-        Err(err) => stop(err),
-        Ok(None) => complete(0),
-        Ok(Some(mime_message)) => match parse_mail(mime_message.as_bytes()).and_then(extract) {
-            Err(err) => stop(err),
-            Ok(info) => {
-                println!("{:?}", info);
-                complete(1)
+    if mail.ctype.mimetype == "text/plain" {
+        mail.get_body().map(|s| Some(s))
+    } else if mail.subparts.is_empty() {
+        Ok(None)
+    } else {
+        let parts: Vec<Option<String>> =
+            mail.subparts.iter()
+            .map(|m| body(&m))
+            .filter_map(|result| result.ok())
+            .filter(|o| o.is_some())
+            .collect();
+        
+            if parts.is_empty() {
+                Ok(None)
+            } else {
+                Ok(parts[0].clone())
             }
-        },
-    }
+        }
+
 }
+
 
 fn fetch(settings: &Settings) -> imap::error::Result<Option<String>> {
     let tls = native_tls::TlsConnector::builder().build()?;
