d7920dde8f5d17915c54b0eb8171b29f8d3b03a3
Richard Dallaway <richard@dallaway.com>
2020-01-15 08:40:47 +0000

    add upload of images to dropbox

--- src/dropbox.rs
+++ src/dropbox.rs
@@ -3,23 +3,55 @@ use super::settings::Settings;
 use reqwest;
 use std::error::Error;
 use std::fs::File;
+use std::io;
+use std::path::PathBuf;
 
 pub fn upload(settings: &Settings, post: &PostInfo) -> Result<(), Box<dyn Error>> {
-    let api = "https://content.dropboxapi.com/2/files/upload";
-    let file = File::open(&post.filename)?;
+    let dropbox = Dropbox::new(&settings.dropbox_access_token);
+    let blog_response = dropbox.upload(&post.filename, &post.relative_path)?;
+    dbg!(blog_response);
 
-    let auth = format!("Bearer {}", &settings.dropbox_access_token);
+    for image in post.attachments.iter() {
+        let img_resp = dropbox.upload(&image.file, &image.relative_path)?;
+        dbg!(img_resp);
 
-    let api_args = format!("{{\"path\":\"{}{}\"}}", "/", post.relative_path);
-
-    let request = reqwest::blocking::Client::new()
-        .post(api)
-        .header("Content-Type", "application/octet-stream")
-        .header("Authorization", auth)
-        .header("Dropbox-API-Arg", api_args)
-        .body(file);
-    let response = request.send()?;
-    println!("{:?}", response);
+        let thumb = &image.thumbnail;
+        let thumb_resp = dropbox.upload(&thumb.file, &thumb.relative_path)?;
+        dbg!(thumb_resp);
+    }
 
     Ok(())
+}
+
+struct Dropbox {
+    auth_header: String,
+}
+
+impl Dropbox {
+    fn new(token: &String) -> Dropbox {
+        let auth_header = format!("Bearer {}", token);
+        Dropbox { auth_header }
+    }
+
+    fn upload(&self, filename: &PathBuf, dropbox_path: &String) -> Result<(), Box<dyn Error>> {
+        let file = File::open(filename)?;
+
+        let api_args = format!("{{\"path\":\"{}{}\"}}", "/", dropbox_path);
+
+        let request = reqwest::blocking::Client::new()
+            .post("https://content.dropboxapi.com/2/files/upload")
+            .header("Content-Type", "application/octet-stream")
+            .header("Authorization", &self.auth_header)
+            .header("Dropbox-API-Arg", api_args)
+            .body(file);
+
+        let resp = request.send()?;
+        match resp.status() {
+            reqwest::StatusCode::OK => Ok(()),
+            code => Err(Box::new(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Expected 200, not {}", code),
+            ))),
+        }
+    }
 }
